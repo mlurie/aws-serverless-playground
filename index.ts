16f25b016b1f567bc56cebf4dc0851a07aa125cc
mlurie <melurie03@gmail.com>
2018-05-19 23:58:17 -0500

    cleaning up lambda

--- index.ts
+++ index.ts
@@ -4,23 +4,22 @@ import {
   APIGatewayProxyEvent,
   APIGatewayProxyHandler,
   APIGatewayProxyResult,
-  Callback, Context,
-  Handler,
 } from "aws-lambda";
 import {
   DocumentClient,
 } from "aws-sdk/clients/dynamodb";
 
-const dynamo = new DocumentClient();
+const dynamo: DocumentClient = new DocumentClient();
 
-export const handler: APIGatewayProxyHandler = (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const result: APIGatewayProxyResult = {
     statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin" : "*",
       "Access-Control-Allow-Credentials" : true,
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ message: "Hello World!" }),
   };
-  return Promise.resolve(result);
+  return await Promise.resolve(result);
 };
