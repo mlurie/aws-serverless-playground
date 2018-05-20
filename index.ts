54a00c92e3e59a32fb97b7818d734e098df0f18e
mlurie <melurie03@gmail.com>
2018-05-20 16:20:38 -0500

    adding dynamodb usage and test

--- index.ts
+++ index.ts
@@ -7,11 +7,21 @@ import {
 } from "aws-lambda";
 import {
   DocumentClient,
+  GetItemOutput,
 } from "aws-sdk/clients/dynamodb";
-
-const dynamo: DocumentClient = new DocumentClient();
+import {
+  AWSError,
+} from "aws-sdk/lib/error";
+import {
+  PromiseResult,
+} from "aws-sdk/lib/request";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const dynamo: DocumentClient = new DocumentClient();
+  const data: PromiseResult<GetItemOutput, AWSError> = await dynamo.get({
+    TableName: "TestTable",
+    Key: { Key: "Apple" },
+  }).promise();
   const result: APIGatewayProxyResult = {
     statusCode: 200,
     headers: {
@@ -19,7 +29,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       "Access-Control-Allow-Credentials" : true,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ message: "Hello World!" }),
+    body: JSON.stringify(data.Item),
   };
-  return await Promise.resolve(result);
+  return await result;
 };
--- tests/handler.test.js
+++ tests/handler.test.js
@@ -1,6 +1,24 @@
-const { handler } = require('../dist/index');
+const { handler } = require("../dist/index");
+const AWS = require("aws-sdk-mock");
 
-test('handler', async () => {
-  const output = await handler({});
-  expect(output.statusCode).toEqual(200);
+describe("handler tests", () => {
+  beforeAll(() => {
+    AWS.mock("DynamoDB.DocumentClient", "get", (params, callback) => {
+      callback(null, {Item: {Key: 'Value'}});
+    });
+  });
+
+  afterAll(() => {
+    AWS.restore("DynamoDB.DocumentClient");
+  });
+
+  test("status code success", async () => {
+    const output = await handler({});
+    expect(output.statusCode).toEqual(200);
+  });
+
+  test("body success", async () => {
+    const output = await handler({});
+    expect(output.body).toEqual(JSON.stringify({Key: 'Value'}));
+  });
 });
