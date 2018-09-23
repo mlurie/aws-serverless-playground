878ef82092aee71c7fd1c22188f7d41c699215a0
mlurie <melurie03@gmail.com>
2018-09-22 20:48:32 -0400

    upgrading typescript

--- webpack.common.js
+++ webpack.common.js
@@ -15,10 +15,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(tsx?)|(js)$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
   },
 };
